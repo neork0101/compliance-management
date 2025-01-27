f8a8d788d0d893d07c0d8dfcb43c8ae8ce2fabe1
Robin <neork0101@gmail.com>
2025-01-27 09:12:57 -0500

    Update user modified

--- src/main/java/com/in/auth/controller/UserController.java
+++ src/main/java/com/in/auth/controller/UserController.java
@@ -25,71 +25,76 @@ import com.in.security.models.User;
 @RequestMapping("identitymanagement/api/user")
 public class UserController {
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
-	
+
 	@Autowired(required = true)
-	 private UserRepository userRepository;
-	
+	private UserRepository userRepository;
+
 	@Autowired(required = true)
 	private RoleRepository roleRepository;
-	
-	 @GetMapping("/{id}")
-	 // @PreAuthorize("hasRole('MODERATOR')")
-	  public Optional<User> getUserById(@PathVariable String id) {
-		 log.info("Start getUserById: {}");
-	    return userRepository.findById(id);
-	  }
-	 
-	 @GetMapping("/search")
-	 // @PreAuthorize("hasRole('ADMIN')")
-	  public List<User> getAllUser() {
-		 log.info("Start getAllUser: {}");
-	    return userRepository.findAll();
-	  }
-	 
-	 @DeleteMapping("/{id}")
-	 // @PreAuthorize("hasRole('ADMIN')")
-	  public String deleteUser(@PathVariable String id) {
-		 log.info("Start deleteUser: {}");
-	    userRepository.deleteById(id);
-	    log.info("Success deleteUser:");
-	    return "User Deletion Successful";
-	  }
-	 
-	 @GetMapping("/roles")
-	 // @PreAuthorize("hasRole('ADMIN')")
-	  public List<Role> getAllRole() {
-		 log.info("Start getAllRole: {}");
-	    return roleRepository.findAll();
-	  }
-	 
-	 @DeleteMapping("/role/{id}")
-	 // @PreAuthorize("hasRole('ADMIN')")
-	  public String deleteRole(@PathVariable String id) {
-		 log.info("Start deleteRole: {}");
-		 roleRepository.deleteById(id);
-	    log.info("Success deleteRole:");
-	    return "Role Deletion Successful";
-	  }
-	 
- 	@PutMapping("/{id}")
-    // @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<String> updateUser(@PathVariable String id, @RequestBody User updatedUser) {
-        log.info("Start updateUser: {}", id);
-        
-        return userRepository.findById(id).map(user -> {
-        	
-            user.setUsername(updatedUser.getUsername());
-            user.setEmail(updatedUser.getEmail());
-            user.setRoles(updatedUser.getRoles());
-            // Update any other relevant fields here
-            userRepository.save(user);
-            log.info("Success updateUser: {}", id);
-            return ResponseEntity.ok().body("Successfully updated");
-        }).orElseGet(() -> {
-            log.warn("User not found: {}", id);
-           return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
-            
-        });
-    }
+
+	@GetMapping("/{id}")
+	// @PreAuthorize("hasRole('MODERATOR')")
+	public Optional<User> getUserById(@PathVariable String id) {
+		log.info("Start getUserById: {}");
+		return userRepository.findById(id);
+	}
+
+	@GetMapping("/search")
+	// @PreAuthorize("hasRole('ADMIN')")
+	public List<User> getAllUser() {
+		log.info("Start getAllUser: {}");
+		return userRepository.findAll();
+	}
+
+	@DeleteMapping("/{id}")
+	// @PreAuthorize("hasRole('ADMIN')")
+	public String deleteUser(@PathVariable String id) {
+		log.info("Start deleteUser: {}");
+		userRepository.deleteById(id);
+		log.info("Success deleteUser:");
+		return "User Deletion Successful";
+	}
+
+	@GetMapping("/roles")
+	// @PreAuthorize("hasRole('ADMIN')")
+	public List<Role> getAllRole() {
+		log.info("Start getAllRole: {}");
+		return roleRepository.findAll();
+	}
+
+	@DeleteMapping("/role/{id}")
+	// @PreAuthorize("hasRole('ADMIN')")
+	public String deleteRole(@PathVariable String id) {
+		log.info("Start deleteRole: {}");
+		roleRepository.deleteById(id);
+		log.info("Success deleteRole:");
+		return "Role Deletion Successful";
+	}
+
+	@PutMapping("/{id}")
+	// @PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<String> updateUser(@PathVariable String id, @RequestBody User updatedUser) {
+		log.info("Start updateUser: {}", id);
+
+		return userRepository.findById(id).map(user -> {
+
+			user.setUsername(updatedUser.getUsername());
+			// user.setEmail(updatedUser.getEmail());
+			//user.setStatus("Active");
+			if (updatedUser.getRoles() != null)
+				user.setRoles(updatedUser.getRoles());
+
+			if (updatedUser.getOrganization() != null)
+				user.setOrganization(updatedUser.getOrganization());
+			// Update any other relevant fields here
+			user = userRepository.save(user);
+			log.info("Success updateUser: {}", id);
+			return ResponseEntity.ok().body("Successfully updated");
+		}).orElseGet(() -> {
+			log.warn("User not found: {}", id);
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+
+		});
+	}
 
 }
